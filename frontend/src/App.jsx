689c393b8727bd809b3f1ee051a27452fd712ca1
ing1847 <thanakon.dat@gmail.com>
2025-11-08 16:51:14 +0700

    member

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,24 +4,34 @@ import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
 import SongList from "./home/SongList";
 import UploadSong from "./upload/uploads";
+import Primium from "./primium/member";
+import Profile from "./Profiles/Profile";
+
+
 
 function App() {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <div>
+
       <Navbar />
 
       {/* ✅ ส่ง setSearchTerm ลงไป */}
-      <HeroSection setSearchTerm={setSearchTerm} />
+      {/* <HeroSection setSearchTerm={setSearchTerm} /> */}
 
-      {/* ✅ ส่ง searchTerm ลงไป */}
-      <SongList searchTerm={searchTerm} />
+      {/* ✅ ส่ง searchTerm ลงไป
+      <SongList searchTerm={searchTerm} /> */}
 
+      <Primium />
 
       <Footer />
- 
-      <UploadSong />
+      {/* <UploadSong /> */}
+
+       
+
+      {/* <Profile /> */}
+
 
 
     </div>
