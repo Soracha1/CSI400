701f999836698a256caf81ba62ded05f3055745c
Soracha1 <rewzazx@gmail.com>
2025-11-15 18:36:54 +0700

    server

--- backend/server.js
+++ backend/server.js
@@ -337,13 +337,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 
-
-
-
-// app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
-
-app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
-
 // =========================
 // TAGS ดึงแท็กทั้งหมด
 app.get("/api/tags", async (req, res) => {
